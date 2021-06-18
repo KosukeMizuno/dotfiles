56414a1e1479118ed596b62d4ad26b04d662499f
Kosuke Mizuno <dotmapu@gmail.com>
2021-06-19 05:00:04 +0900

    ittan

--- bin/install.sh
+++ bin/install.sh
@@ -201,7 +201,7 @@ if ${DOTINSTALL_RUST:-true}; then
     install_cargo bat
     install_cargo evcxr evcxr_repl
 
-    if [[ -z $(comand -v rust-analyzer)]]; then
+    if [[ -z $(command -v rust-analyzer) ]]; then
         if [[ ! -d "$PREFIX/src/rust-analyzer" ]]; then
             git clone https://github.com/rust-analyzer/rust-analyzer.git "$PREFIX/src/rust-analyzer"
         fi
---
+++ windows/README.md
@@ -0,0 +1,19 @@
+# win10 セットアップメモ
+
+## GUIでやること
+
+- cap2ctrl を入れる
+- IMEの設定
+  - 常に半角スペース
+  - Ctrl+SpaceでIME切り替え
+
+## 開発環境セットアップ
+
+途中
+参考：[MSYS2でWindowsのターミナル環境構築 | HY Techs](https://hyrorre.com/post/winterminal/#i-3)
+
+- Install msys2
+  - Windows環境変数に`HOME=C:\Users\<username>`を追加する
+- pacman
+  - sync & upgrade -- `pacman -Syuu`
+
