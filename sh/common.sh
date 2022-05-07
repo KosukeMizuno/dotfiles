d32fa20a0ab3a44ce5b34d3eb71f078ed7ce433b
Kosuke Mizuno <dotmapu@gmail.com>
2022-05-07 16:47:13 +0900

    detect env

--- sh/common.sh
+++ sh/common.sh
@@ -4,3 +4,13 @@
 # terminal config
 export USE_ICON_IN_TERM=${USE_ICON_IN_TERM:-true}
 
+# detect wsl/mingw/native linux
+export IS_WSL=false IS_MINGW=false IS_NATIVE_LINUX=false
+proc_version=$(cat /proc/version | awk '{print tolower($0)}')
+if [[ "$proc_version" =~ "microsoft" ]]; then
+    export IS_WSL=true
+elif [[ "$proc_version" =~ "mingw" ]]; then
+    export IS_MINGW=true
+else
+    export IS_NATIVE_LINUX=true
+fi
